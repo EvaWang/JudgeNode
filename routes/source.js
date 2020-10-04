d8d754b6d17fed08ab10aaa0e9e368a483b1e4f8
r08525069 <r08525069@ntu.edu.tw>
2020-10-04 18:03:17 +0800

    student can only read their own sources.

--- routes/source.js
+++ routes/source.js
@@ -5,43 +5,67 @@ var multer = require('multer');
 var _config = require('../lib/config').config;
 var markdown = require('../lib/components/plugin/markdown');
 var fs = require('fs');
+var utils = require('../lib/components/utils');
+var loginURL = utils.url_for('login');
 
 router.get('/:sid', function(req, res, next) {
     var sid = req.params.sid;
-    dblink.submission.source_code(sid, req.session.uid, req.session["class"], function(source_code) {
-        res.type('text/plain');
-        var text = '';
-        for (var i in source_code)
-            text += source_code[i].code + '\n';
-        res.send(text);
-    });
+    var uid = req.session.uid;
+
+    dblink.helper.canReadAns(sid, uid).then(function(isPass){
+        if(isPass){
+            dblink.submission.source_code(sid, req.session.uid, req.session["class"], function(source_code) {
+                res.type('text/plain');
+                var text = '';
+                for (var i in source_code)
+                    text += source_code[i].code + '\n';
+                res.send(text);
+            });
+        }else{
+            res.redirect(loginURL);
+        }
+    },function(err){
+        // error handler
+        res.redirect(loginURL);
+    })
+    
 });
 router.get('/highlight/:sid', function(req, res, next) {
     var sid = req.params.sid;
     var uid = req.session.uid;
-    dblink.helper.isAdmin(uid, function(isadmin) {
-        dblink.submission.source_result(sid, req.session.uid, req.session["class"], function(source_result_json) {
-            dblink.submission.source_code(sid, req.session.uid, req.session["class"], function(source_code) {
-                var text = '';
-                for (var i in source_code) {
-                    text += '## ' + source_code[i].title + ' ##\n';
-                    text += '```cpp\n' + source_code[i].code + '```\n';
-                }
-                dblink.submission.list({
-                    sid: sid
-                }, isadmin, function(slist) {
-                    res.render('layout', {
-                        layout: 'highlight',
-                        user: req.session,
-                        sid: sid,
-                        source_result: source_result_json,
-                        html_code: markdown.post_marked(text),
-                        subs_info: slist && slist.length > 0 ? slist[0] : null
+
+    dblink.helper.canReadAns(sid, uid).then(function(isPass){
+        var isAdmin = req.session["class"] == null;
+        if(isPass){
+            dblink.submission.source_result(sid, req.session.uid, req.session["class"], function(source_result_json) {
+                dblink.submission.source_code(sid, req.session.uid, req.session["class"], function(source_code) {
+                    var text = '';
+                    for (var i in source_code) {
+                        text += '## ' + source_code[i].title + ' ##\n';
+                        text += '```cpp\n' + source_code[i].code + '```\n';
+                    }
+                    dblink.submission.list({
+                        sid: sid
+                    }, isAdmin, function(slist) {
+                        res.render('layout', {
+                            layout: 'highlight',
+                            user: req.session,
+                            sid: sid,
+                            source_result: source_result_json,
+                            html_code: markdown.post_marked(text),
+                            subs_info: slist && slist.length > 0 ? slist[0] : null
+                        });
                     });
                 });
             });
-        });
-    });
+        }else{
+            res.redirect(loginURL);
+        }
+    },function(err){
+        // error handler
+        res.redirect(loginURL);
+    })
+
 });
 
 module.exports = router;
